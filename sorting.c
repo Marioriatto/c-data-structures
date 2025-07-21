1f53712ea66c9e9cc57618aff1253fa59b7b818d
Mario Rodríguez <mariocrg3@gmail.com>
2025-07-21 14:23:56 -0600

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -3,7 +3,7 @@
 #include<string.h>
 int main()
 {
-string harryPotter[] = {"Hedwig", "Dumbledore", "Cedrick", "Sirius", "Lupin", "Snape", "Fred", "Dobby"}, LoTR[] = {"Aragorn","Sam","Frodo","Gandalf","Legolas","Gimli","Gollum","Bilbo"};
+string harryPotter[8] = {"Hedwig", "Dumbledore", "Cedrick", "Sirius", "Lupin", "Snape", "Fred", "Dobby"}, LoTR[8] = {"Aragorn","Sam","Frodo","Gandalf","Legolas","Gimli","Gollum","Bilbo"};
 
 }
 string bubbleSort(string[] list)
@@ -15,7 +15,7 @@ string bubbleSort(string[] list)
     cambios = false;
     for (int i = 0; i < len; i++)
     {
-        int ans = get_int("1: %s o 2: %s?",list[i],list[i + 1]);
+        int ans = get_int("Quien es mejor?\n1: %s o 2: %s\n?",list[i],list[i + 1]);
         if (ans == 2)
         {
             string temp = list[i];
@@ -26,4 +26,21 @@ string bubbleSort(string[] list)
     }
     }while(cambios);
 return list;
+}
+string mergeSort(int len, string[] list1, string[] list2)
+{
+    string newList[len * 2];
+    for (int i = 0; i < len; i++)
+    {
+        int ans = get_int("Quien es mejor?\n1: %s o 2: %s\n");
+        if (ans == 1)
+        {
+            newList[i] = list1[i];
+        }
+        else
+        {
+            newList[i] = list2[i];
+        }
+    }
+    return newList;
 }
