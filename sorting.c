a72ea6798a638d2000cdfd7eed277bf099a42f58
Mario Rodríguez <mariocrg3@gmail.com>
2025-07-20 18:43:18 -0600

    Create sorting.c

---
+++ sorting.c
@@ -0,0 +1,7 @@
+#include<cs50.h>
+#include<stdio.h>
+int main()
+{
+string harryPotter[] = {"Hedwig", "Dumbledore", "Cedrick", "Sirius", "Lupin", "Snape", "Fred", "Dobby"}, señorDeLosAnillos[] = {"Aragorn","Sam","Frodo","Gandalf","Legolas","Gimli","Gollum","Bilbo"};
+
+}
