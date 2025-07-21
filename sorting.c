3154e5e3df23f59bcca90586625617c0c9c6592f
Mario Rodríguez <mariocrg3@gmail.com>
2025-07-21 09:48:40 -0600

    Update with bubble sort sorting.c

--- sorting.c
+++ sorting.c
@@ -1,7 +1,27 @@
 #include<cs50.h>
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-string harryPotter[] = {"Hedwig", "Dumbledore", "Cedrick", "Sirius", "Lupin", "Snape", "Fred", "Dobby"}, señorDeLosAnillos[] = {"Aragorn","Sam","Frodo","Gandalf","Legolas","Gimli","Gollum","Bilbo"};
+string harryPotter[] = {"Hedwig", "Dumbledore", "Cedrick", "Sirius", "Lupin", "Snape", "Fred", "Dobby"}, LoTR[] = {"Aragorn","Sam","Frodo","Gandalf","Legolas","Gimli","Gollum","Bilbo"};
 
+}
+string bubbleSort(string[] list)
+{
+    int len = strlen(list);
+    bool cambios = false;
+    do
+    {
+    for (int i = 0; i < len; i++)
+    {
+        int ans = get_int("1: %s o 2: %s?",list[i],list[i + 1]);
+        if (ans == 2)
+        {
+            string temp = list[i];
+            list[i] = list[i+1];
+            list[i + 1] = temp;
+            cambios = true;
+        }
+    }
+    }while(cambios);
 }
